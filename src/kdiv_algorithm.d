ea4861459389a3dee656bff380d80b9f031744aa
Kai D. Gonzalez <gkai70264@gmail.com>
2022-05-13 12:58:45 -0400

    first commit

---
+++ src/kdiv_algorithm.d
@@ -0,0 +1,53 @@
+module kdiv;
+
+import std.stdio;
+import std.algorithm;
+
+// "one" is the thing to be divided.
+// This function only supports basic division algorithm.
+float basic_division(float one, float two)
+{
+    float p = 0;
+    int t = 0;
+
+    // we need to put p into "one" until it's equal
+    while (p < one)
+    {
+        // we add "p" by the number
+        p += two;
+        // add a time
+        t += 1;
+
+        // if we've reached it
+        if (p == one)
+        {
+            break;
+        }
+        else if (p > one)
+        {
+            // Hey no remainders!
+            writeln("This function does not support remainders.");
+            return -1.00000;
+        }
+    }
+
+    return t;
+}
+
+float rem_division_v2(float one, float two)
+{
+    float p;
+    p = 0;
+    int times;
+    times = 0;
+
+    while (p < one) {
+        p += two;
+
+        times += 1;
+
+        if (p == one) { break; } else if (p > one) { return p - one; }
+    }
+    return times;
+}
+
