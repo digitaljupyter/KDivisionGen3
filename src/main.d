235efa4376ded8a0b5f4dcb9a8901a0e365eacb7
Kai Daniel Gonzalez <gkai70264@gmail.com>
2022-12-17 18:20:11 -0500

    Categorise test file

--- src/main.d
+++ src/main.d
@@ -14,18 +14,27 @@ module main;
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import kdiv;
+import kmath_algorithm;
 import std.stdio;
 
 void main()
 {
+    writeln("[----- DIVISION (no remainders) (old) -----]");
+    import std.conv : to;
     float e = basic_division(12, 3);
     float b = 12 / 3;
-    writeln(e);
-    writeln(b);
-
+    writefln("KMath implmentation: %d", to!int(e));
+    writefln("D implementation: %f", b);
+    
+    writeln("[----- DIVISION (remainders) -----]");
     float a = 9 / 5;
     float c = rem_division_v2(9, 5);
-    writeln(a);
-    writeln(c);
+    writefln("D implementation: %d", to!int(a));
+    writefln("KMath implementation: %d", to!int(c));
+
+    writeln("[----- MULTIPLICATION (x * y) -----]");
+    float x = 6 * 8;
+    float y = basic_multiplication(6, 8);
+    writefln("D implementation: %d", to!int(x));
+    writefln("KMath implementation: %d", to!int(y));
 }
